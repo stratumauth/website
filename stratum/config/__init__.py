6adbcad2275f22a199f21f14bf9aea07c18df583
Jamie Hodgson <jamie-hodgson@live.com>
2024-10-12 11:51:19 +0200

    change repo urls

--- stratum/config/__init__.py
+++ stratum/config/__init__.py
@@ -4,12 +4,12 @@
 
 class BaseConfig(object):
     APP_RELEASE_INFO_URL = (
-        "https://api.github.com/repos/jamie-mh/AuthenticatorPro/releases/latest"
+        "https://api.github.com/repos/stratumauth/app/releases/latest"
     )
     ICON_PACK_RELEASE_INFO_URL = (
-        "https://api.github.com/repos/jamie-mh/AuthenticatorProIcons/releases/latest"
+        "https://api.github.com/repos/stratumauth/icons/releases/latest"
     )
-    WIKI_FILE_URL = "https://raw.githubusercontent.com/wiki/jamie-mh/AuthenticatorPro"
+    WIKI_FILE_URL = "https://raw.githubusercontent.com/wiki/stratumauth/app"
     MARKDOWN_FILE_URL = (
-        "https://raw.githubusercontent.com/jamie-mh/AuthenticatorPro/master"
+        "https://raw.githubusercontent.com/stratumauth/app/master"
     )
